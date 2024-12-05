8810efb7d12673f8174e8617d8a7556cecc1e818
Piyush Dahiwadkar <97776547+Piyush2627@users.noreply.github.com>
2024-12-05 23:31:33 +0530

    navigation change

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,44 @@
+import { useEffect, useRef, useState } from "react";
 import HabitsPage from "../pages/HabitPage";
+import React from "react";
 
 function MainLayout() {
+  const [sideNavBar, setSideNavBar] = React.useState(false);
+  const sideNavRef = useRef<HTMLDivElement>(null);
+
+  const handleClick = () => {
+    setSideNavBar(!sideNavBar);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        sideNavRef.current &&
+        !sideNavRef.current.contains(event.target as Node)
+      ) {
+        setSideNavBar(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [sideNavRef]);
+
   return (
     <div className="flex h-screen w-screen overflow-hidden bg-zinc-900">
-      <div className="my-5 w-60 rounded-r-3xl bg-white p-5 opacity-10">
+      <div className={` ${sideNavBar ? "hidden" : "flex"}`}>
+        <div className="my-5 w-60 rounded-r-3xl bg-white p-5 opacity-10">
+          <button
+            onClick={handleClick}
+            className="rounded-md hover:bg-gray-200 lg:px-2 lg:py-1"
+          >
+            hello
+          </button>
+        </div>
+        <button className="border bg-yellow-50 p-3">hi</button>
         hello
       </div>
       <div className="grow">
