a7510732afbb63fb847fcb0affd8c4c44648e626
Piyush Dahiwadkar <97776547+Piyush2627@users.noreply.github.com>
2024-12-07 23:36:07 +0530

    Updating the side navigation bar

---
+++ src/components/ui/SideNavigation.tsx
@@ -0,0 +1,9 @@
+function SideNavigation() {
+  return (
+    <>
+      <div className="rounded bg-white p-4">Side Navigation Container </div>
+    </>
+  );
+}
+
+export default SideNavigation;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,10 @@
-import { useEffect, useRef, useState } from "react";
-import HabitsPage from "../pages/HabitPage";
+import { useEffect, useRef } from "react";
 import React from "react";
+//icons
+import { TbMenu } from "react-icons/tb";
+//components
+import HabitsPage from "../pages/HabitPage";
+import SideNavigation from "../components/ui/SideNavigation";
 
 function MainLayout() {
   const [sideNavBar, setSideNavBar] = React.useState(false);
@@ -29,8 +33,28 @@ function MainLayout() {
 
   return (
     <div className="flex h-screen w-screen overflow-hidden bg-zinc-900">
+      {sideNavBar && (
+        <div className="group absolute left-2 top-2 rounded bg-white p-2">
+          <button
+            className="flex items-center space-x-2 text-sm"
+            onClick={handleClick}
+          >
+            <TbMenu />
+          </button>
+          <div className="hidden group-hover:block">
+            <div className="absolute left-0 z-20">
+              <div className="w-72 rounded-xl bg-white p-4 opacity-100">
+                <div className="mb-2 rounded bg-black p-2 text-white">
+                  Create a new Page in this section
+                </div>
+                <SideNavigation />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
       <div className={` ${sideNavBar ? "hidden" : "flex"}`}>
-        <div className="my-5 w-60 rounded-r-3xl bg-white p-5 opacity-10">
+        <div className="w-60 bg-black p-5">
           <button
             onClick={handleClick}
             className="rounded-md hover:bg-gray-200 lg:px-2 lg:py-1"
@@ -38,8 +62,6 @@ function MainLayout() {
             hello
           </button>
         </div>
-        <button className="border bg-yellow-50 p-3">hi</button>
-        hello
       </div>
       <div className="grow">
         <div className="mx-auto w-2/5 rounded-b-[90px] bg-white p-2 text-center">
